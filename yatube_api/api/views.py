0a760b275b10a2320c0ffee9dc8115a3a63f0ef6
Andrey Yastrebov <geeksogen@gmail.com>
2022-05-13 20:37:38 +0300

    v2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -63,7 +63,7 @@ class FollowViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.request.user.username)
-        return user.follower
+        return user.follower.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
